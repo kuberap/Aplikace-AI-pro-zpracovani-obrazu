820ae3923a61e71ef3075e739ae90dedbb92d81c
kuberap <47250449+kuberap@users.noreply.github.com>
2024-09-03 16:46:52 +0200

    Update generace-videa.py

--- GrayCrop/generace-videa.py
+++ GrayCrop/generace-videa.py
@@ -1,1 +1,37 @@
+import os
+import cv2
+from PIL import Image
+
+adresar = "./Images/"
+
+output_filename = "detekovano.avi"
+
+# Nastavení video kodeku a parametry výstupního videa
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fps = 5  # Počet snímků za sekundu
+video_size = None
+
+# Seznam pro uložení všech obrázků
+obrazky = []
+
+# Načtení všech obrázků z adresáře
+for soubor in sorted(os.listdir(adresar)):
+    if soubor.endswith(('.jpg', )):
+        cesta_k_obrazku = os.path.join(adresar, soubor)
+        obrazek = Image.open(cesta_k_obrazku)
+        if video_size is None:
+            video_size = obrazek.size
+        obrazky.append(cesta_k_obrazku)
+
+# Vytvoření video spisovače
+video_writer = cv2.VideoWriter(output_filename, fourcc, fps, video_size)
+
+# Přidání obrázků do videa
+for obrazek_path in obrazky:
+    obrazek = cv2.imread(obrazek_path)
+    video_writer.write(obrazek)
+
+# Uvolnění video spisovače
+video_writer.release()
+
 
